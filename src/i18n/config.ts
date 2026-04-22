43a3a56c1d6f55d2247c34e3b39b9a9cf6ca453f
Vedaansh Vishwakarma <vedaanshvishwakarma896@gmail.com>
2026-04-22 19:36:20 +0530

    Update config.ts

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -14,7 +14,6 @@ const resources = {
         contact: 'Contact'
       },
       hero: {
-        welcome: 'Welcome to my portfolio',
         name: 'Vedaansh Vishwakarma',
         titles: [
           'Full Stack Developer',
